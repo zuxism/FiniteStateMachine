9b1cad2f94971d08819d449727ce7b4c23405c2a
zuxism <93733961+zuxism@users.noreply.github.com>
2022-03-09 18:58:13 +0100

    Update fsm.h

--- fsm.h
+++ fsm.h
@@ -146,7 +146,7 @@ void fsm_set_tranzition(const char* from_this_state_name, const char* to_this_st
 
 
 const char* fsm_active_state() {
-    return active_state_info.name;
+    return active_state_info->state_name;
 }
 
 
